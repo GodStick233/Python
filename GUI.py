70a32ff8c22ea2d0f0694692e6c44325c359a6d2
a15641404 <499321348@qq.com>
2017-09-27 13:53:58 +0800

    2

--- GUI.py
+++ GUI.py
@@ -1,5 +1,12 @@
 import tkinter
+def callback():
+    var.set("hhhhhhha")
 root = tkinter.Tk()
+frame1=tkinter.Frame(root)
+frame2=tkinter.Frame(root)
+var = tkinter.StringVar()
+var.set("wtffffffff")
+textLabel = tkinter.Label(frame1, textvariable =var, justify =LEFT)
 root.title("hello world")
 table = tkinter.Label(root, text="hello world")
 table.pack()
---
+++ game.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+
+pygame.init()
+size = width, height = 600, 400
+speed = [-2, 1]
+bg = (225, 225, 225)
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("hello world")
+
